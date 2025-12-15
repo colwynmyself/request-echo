91f9fc0c2dd1d082e0dc407fe284fb49b1f5e27f
Colwyn Fritze-Moor <colwyn@colwyn.me>
2025-12-15 01:08:12 -0800

    fix: types

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,28 @@
-// @ts-types="npm:@types/express@4.17.15"
+// @ts-types="npm:@types/express"
 import express from "express";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  const headers = req.headers;
+function handleRequest(req: express.Request, res: express.Response) {
   const body = req.body;
+  const headers = req.headers;
+  const method = req.method;
+  const params = req.params;
+  const url = req.url;
 
   res.json({
     body,
     headers,
+    method,
+    params,
+    url,
   });
-});
+}
 
-app.post("/", (req, res) => {
-  const headers = req.headers;
-  const body = req.body;
-
-  res.json({
-    body,
-    headers,
-  });
-});
+app.get("/", handleRequest);
+app.post("/", handleRequest);
 
 app.listen(8000);
 console.log(`Server is running on http://localhost:8000`);
